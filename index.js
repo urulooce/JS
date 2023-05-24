06f615c133fb463e34fd81bdd2fb461dc46fe41c
urulooce <soyler092@gmail.com>
2023-05-24 19:05:31 +0300

    new feature
    new method is added: calculateSum

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-var i = 0;
+/**
+ * calculates sum of two numbers
+ *
+ * @param {Number} a
+ * @param {Number} b
+ * @return {Number} - sum of the numbers
+ */
+function calculateSum(a, b) {
+    return a + b;
+}
+
+// calculate the result
+const sum = calculateSum(1, 2);
+
+console.log(sum);
