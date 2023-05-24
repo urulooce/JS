6f12a2362b30bccf5b5fcd0f2dd272b07dbf0900
urulooce <soyler092@gmail.com>
2023-05-24 19:14:44 +0300

    Revert "Merge pull request #1 from urulooce/development"
    This reverts commit 22d7b30941e43ba894f2124dd5f0d527e8a3ce3f, reversing
    changes made to bfc2406778bda1f9bf7a6acfde7d77ee1918ef13.

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
 
-/**
- * calculates sum of two numbers
- *
- * @param {Number} a
- * @param {Number} b
- * @return {Number} - sum of the numbers
- */
-function calculateSum(a, b) {
-    return a + b;
-}
-
-// calculate the result
-const sum = calculateSum(1, 2);
-
-// printing value
-console.log(sum);
+var i = 0;
